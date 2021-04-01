3e5f77b9fbf8769236446dfb436b9d1b9c0f54bc
Marcus Wong <marcus.wong@mycit.ie>
2021-04-01 11:31:44 +0100

    Animation header fix

--- js/custom.js
+++ js/custom.js
@@ -118,40 +118,40 @@ $(document).ready(function() {
 
 // validate email form
 
-const constraints = {
-    name: {
-        presence: { allowEmpty: false }
-    },
-    email: {
-        presence: { allowEmpty: false },
-        email: true
-    },
-    message: {
-        presence: { allowEmpty: false }
-    }
-};
+// const constraints = {
+//     name: {
+//         presence: { allowEmpty: false }
+//     },
+//     email: {
+//         presence: { allowEmpty: false },
+//         email: true
+//     },
+//     message: {
+//         presence: { allowEmpty: false }
+//     }
+// };
 
-const form = document.getElementById('contactForm');
+// const form = document.getElementById('contactForm');
 
-form.addEventListener('submit', function (event) {
-  const formValues = {
-      name: form.elements.name.value,
-      email: form.elements.email.value,
-      message: form.elements.message.value
-  };
+// form.addEventListener('submit', function (event) {
+//   const formValues = {
+//       name: form.elements.name.value,
+//       email: form.elements.email.value,
+//       message: form.elements.message.value
+//   };
 
-  const errors = validate(formValues, constraints);
+//   const errors = validate(formValues, constraints);
 
-  if (errors) {
-    event.preventDefault();
-    const errorMessage = Object
-        .values(errors)
-        .map(function (fieldValues) { return fieldValues.join(', ')})
-        .join("\n");
+//   if (errors) {
+//     event.preventDefault();
+//     const errorMessage = Object
+//         .values(errors)
+//         .map(function (fieldValues) { return fieldValues.join(', ')})
+//         .join("\n");
 
-    alert(errorMessage);
-  }
-}, false);
+//     alert(errorMessage);
+//   }
+// }, false);
 
 
 // Animation for header when hover
